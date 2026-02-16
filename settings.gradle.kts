70f590fbcc31b18087b98e2daf8f0a22735e535d
Dani Cano Nicolau <daniel.canonicolau@externos.plexus.es>
2026-02-16 17:49:22 +0100

    .gitignore graddle settings

---
+++ settings.gradle.kts
@@ -0,0 +1,30 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    plugins {
+        id("org.jetbrains.kotlin.android") version "2.0.0"
+        id("org.jetbrains.kotlin.jvm") version "2.0.0"
+        id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" // 👈 importante
+        id("dagger.hilt.android.plugin")
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MusicToMe"
+include(":app")
+ 
