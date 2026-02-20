f7eb8262af7115031e0a90a5057b3fac7c80bddc
Dani Cano Nicolau <daniel.canonicolau@externos.plexus.es>
2026-02-20 22:27:28 +0100

    MusicToMe-4 Pulsar o ir atras dentro de la app falla Fixes #4

--- app/src/main/java/com/myown/musictome/ui/MainActivity.kt
+++ app/src/main/java/com/myown/musictome/ui/MainActivity.kt
@@ -4,17 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.myown.musictome.player.MusicPlayerHandler
 import com.myown.musictome.ui.screens.SongListScreen
 import com.myown.musictome.ui.theme.MusicToMeTheme
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    @Inject
-    lateinit var playerHandler: MusicPlayerHandler
-
     // Lanzador del selector de carpeta
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,7 +22,6 @@ class MainActivity : ComponentActivity() {
     }
 
     override fun onDestroy() {
-        playerHandler.releaseController()
         super.onDestroy()
     }
 }
